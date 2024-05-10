8d90a5117b7a03ec3c535b8a0525e8923f99cfa6
Luiscastillo15 <luisangelcastillo2001@gmail.com>
2024-05-10 16:49:06 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -57,26 +57,26 @@ function App() {
         </div>
       </section>
 
-      <section class="text-gray-600 body-font bg-slate-600" id="tema2">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h2 class="consola text-center font-semibold mb-8 text-xl text-gray-900 tracking-widest font-mono">
+      <section className="text-gray-600 body-font bg-slate-600" id="tema2">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="consola text-center font-semibold mb-8 text-xl text-gray-900 tracking-widest font-mono">
               TIPOS DE BASE DE DATOS
             </h2>
           </div>
-          <div class="flex flex-wrap -m-4">
-            <div class="p-4 lg:w-1/2">
-              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-1/2">
+              <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img
                   alt="team"
-                  class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
+                  className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
                   src="./src/imagenes/bddrelacionales.jpg"
                 />
-                <div class="flex-grow sm:pl-8">
-                  <h2 class="title-font font-medium text-lg text-black">
+                <div className="flex-grow sm:pl-8">
+                  <h2 className="title-font font-medium text-lg text-black">
                     Bases de datos relacionales (SGBDR):
                   </h2>
-                  <p class="mb-4 text-justify text-white">
+                  <p className="mb-4 text-justify text-white">
                     Almacenan datos en tablas con filas y columnas, donde cada
                     fila representa un registro y cada columna representa un
                     atributo. Las relaciones entre tablas se establecen mediante
@@ -86,18 +86,18 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="p-4 lg:w-1/2">
-              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+            <div className="p-4 lg:w-1/2">
+              <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img
                   alt="team"
-                  class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
+                  className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
                   src="./src/imagenes/bddNoSQL.jpg"
                 />
-                <div class="flex-grow sm:pl-8">
-                  <h2 class="title-font font-medium text-lg text-black">
+                <div className="flex-grow sm:pl-8">
+                  <h2 className="title-font font-medium text-lg text-black">
                     Bases de datos NoSQL:
                   </h2>
-                  <p class="mb-4 text-justify text-white">
+                  <p className="mb-4 text-justify text-white">
                     No siguen el modelo relacional, lo que las hace más
                     flexibles y escalables para manejar grandes volúmenes de
                     datos no estructurados o semiestructurados. Ejemplos:
@@ -106,18 +106,18 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="p-4 lg:w-1/2">
-              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+            <div className="p-4 lg:w-1/2">
+              <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img
                   alt="team"
-                  class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
+                  className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
                   src="./src/imagenes/bddrientadaobjetos.png"
                 />
-                <div class="flex-grow sm:pl-8">
-                  <h2 class="title-font font-medium text-lg text-black">
+                <div className="flex-grow sm:pl-8">
+                  <h2 className="title-font font-medium text-lg text-black">
                     Bases de datos orientadas a objetos:
                   </h2>
-                  <p class="mb-4 text-justify text-white">
+                  <p className="mb-4 text-justify text-white">
                     Almacenan objetos en lugar de datos simples, encapsulando
                     datos, métodos y comportamiento. Útiles para modelar
                     relaciones complejas entre entidades. Ejemplo: POET.
@@ -125,18 +125,18 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="p-4 lg:w-1/2">
-              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+            <div className="p-4 lg:w-1/2">
+              <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img
                   alt="team"
-                  class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
+                  className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
                   src="./src/imagenes/bddnube.jpg"
                 />
-                <div class="flex-grow sm:pl-8">
-                  <h2 class="title-font font-medium text-lg text-black">
+                <div className="flex-grow sm:pl-8">
+                  <h2 className="title-font font-medium text-lg text-black">
                     Bases de datos en la nube:
                   </h2>
-                  <p class="mb-4 text-justify text-white">
+                  <p className="mb-4 text-justify text-white">
                     Ofrecen almacenamiento y gestión de bases de datos en
                     servidores remotos, accesibles a través de internet.
                     Ejemplos: Amazon RDS, Microsoft Azure SQL Database, Google
@@ -149,20 +149,20 @@ function App() {
         </div>
       </section>
 
-      <section class="text-gray-600 body-font" id="tema3">
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <section className="text-gray-600 body-font" id="tema3">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
             <img
-              class="object-cover object-center rounded"
+              className="object-cover object-center rounded"
               alt="hero"
               src="./src/imagenes/manejadores.png"
             />
           </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 font-mono">
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 font-mono">
               Manejadores o Gestor de Base de Datos (SGBD):
             </h2>
-            <p class="mb-8 leading-relaxed text-justify text-slate-900">
+            <p className="mb-8 leading-relaxed text-justify text-slate-900">
               Un SGBD es un software que permite crear, acceder, administrar y
               controlar una base de datos. Proporciona una interfaz para
               interactuar con la base de datos, definiendo la estructura de
@@ -174,27 +174,27 @@ function App() {
         </div>
       </section>
 
-      <section class="text-gray-600 bg-slate-300 body-font" id="tema4-5">
-        <div class="container px-5 py-24 mx-auto flex flex-wrap">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h2 class="consola text-center font-semibold mb-8 text-xl text-gray-900 tracking-widest font-mono">
+      <section className="text-gray-600 bg-slate-300 body-font" id="tema4-5">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="consola text-center font-semibold mb-8 text-xl text-gray-900 tracking-widest font-mono">
               ENTIDAD - RELACION
             </h2>
           </div>
-          <div class="flex flex-wrap -m-4">
-            <div class="p-4 lg:w-1/2 md:w-full">
-              <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
-                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left gap-5">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-1/2 md:w-full">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left gap-5">
                   <img
                     alt="team"
-                    class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
+                    className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-center sm:mb-0 mb-4"
                     src="./src/imagenes/entidad.png"
                   />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 text-xl title-font font-medium mb-3">
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-xl title-font font-medium mb-3">
                       Entidad:
                     </h2>
-                    <p class="leading-relaxed text-base text-justify">
+                    <p className="leading-relaxed text-base text-justify">
                       En las bases de datos relacionales, una entidad representa
                       un objeto o concepto del mundo real que se desea almacenar
                       y gestionar. Se define como un conjunto de datos que
@@ -205,19 +205,19 @@ function App() {
                 </div>
               </div>
             </div>
-            <div class="p-4 lg:w-1/2 md:w-full">
-              <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
-                <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left gap-5">
+            <div className="p-4 lg:w-1/2 md:w-full">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left gap-5">
                   <img
                     alt="team"
-                    class="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-left sm:mb-0 mb-4"
+                    className="flex-shrink-0 rounded-lg w-48 h-48 object-contain object-left sm:mb-0 mb-4"
                     src="./src/imagenes/relacion.jpg"
                   />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 text-xl title-font font-medium mb-3">
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-xl title-font font-medium mb-3">
                       Relación:
                     </h2>
-                    <p class="leading-relaxed text-base text-justify">
+                    <p className="leading-relaxed text-base text-justify">
                       Una relación representa la asociación entre dos o más
                       entidades en una base de datos relacional. Define cómo se
                       vinculan los datos de una entidad con los de otra. Las
@@ -234,16 +234,16 @@ function App() {
       </section>
 
       <section
-        class="text-gray-600 bg-slate-100 body-font overflow-hidden"
+        className="text-gray-600 bg-slate-100 body-font overflow-hidden"
         id="tema6"
       >
-        <div class="container px-5 py-24 mx-auto">
-          <div class="lg:w-4/5 mx-auto flex flex-wrap">
-            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-              <h2 class="text-gray-900 text-3xl title-font font-medium mb-4 font-mono">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h2 className="text-gray-900 text-3xl title-font font-medium mb-4 font-mono">
                 Atributos:
               </h2>
-              <p class="leading-relaxed mb-4 text-justify text-gray-800">
+              <p className="leading-relaxed mb-4 text-justify text-gray-800">
                 Los atributos son las propiedades o características específicas
                 de una entidad. Cada entidad tiene un conjunto de atributos que
                 la describen y la diferencian de otras entidades. Los atributos
@@ -253,59 +253,59 @@ function App() {
             </div>
             <img
               alt="atributos"
-              class="lg:w-1/2 w-full lg:h-auto h-64 object-contain object-center rounded"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-contain object-center rounded"
               src="./src/imagenes/atributo.png"
             />
           </div>
         </div>
       </section>
 
-      <section class="text-black  bg-slate-600 body-font" id="tema7">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 font-mono">
+      <section className="text-black  bg-slate-600 body-font" id="tema7">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 font-mono">
               Cardinalidad
             </h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-white">
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-white">
               La cardinalidad define la cantidad de instancias de una entidad
               que pueden relacionarse con una o más instancias de otra entidad
               en una base de datos relacional. Existen cuatro tipos de
               cardinalidad:
             </p>
           </div>
-          <div class="flex flex-wrap">
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
+          <div className="flex flex-wrap">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
                 Uno a uno (1:1):
               </h2>
-              <p class="leading-relaxed text-base mb-4 text-white text-justify">
+              <p className="leading-relaxed text-base mb-4 text-white text-justify">
                 Una instancia de una entidad se relaciona con una y solo una
                 instancia de otra entidad.
               </p>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
                 Uno a varios (1:N):
               </h2>
-              <p class="leading-relaxed text-base mb-4 text-white text-justify">
+              <p className="leading-relaxed text-base mb-4 text-white text-justify">
                 Una instancia de una entidad se relaciona con cero, una o más
                 instancias de otra entidad.
               </p>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
                 Varios a uno (N:1):
               </h2>
-              <p class="leading-relaxed text-base mb-4 text-white text-justify">
+              <p className="leading-relaxed text-base mb-4 text-white text-justify">
                 Una o más instancias de una entidad se relacionan con una y solo
                 una instancia de otra entidad.
               </p>
             </div>
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-              <h2 class="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-2xl text-gray-900 font-medium title-font mb-2 font-mono">
                 Varios a varios (N:N):
               </h2>
-              <p class="leading-relaxed text-base mb-4 text-white text-justify">
+              <p className="leading-relaxed text-base mb-4 text-white text-justify">
                 Una o más instancias de una entidad se relacionan con cero, una
                 o más instancias de otra entidad.
               </p>
